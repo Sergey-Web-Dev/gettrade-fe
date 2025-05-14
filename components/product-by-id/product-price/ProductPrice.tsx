4c90ec48a63290c134cac508a2d39eb1d4f882f8
AbzMijon <egorwebdev8@gmail.com>
2025-05-14 16:36:31 +0300

    start do price in product page

--- components/product-by-id/product-price/ProductPrice.tsx
+++ components/product-by-id/product-price/ProductPrice.tsx
@@ -1,5 +1,7 @@
 import { FC } from "react"
 import styles from './product-price.module.scss';
+import Image from "next/image";
+import ActionButton from "@/components/action-button/ActionButton";
 
 interface IProductPrice {
     originalPrice: number;
@@ -16,7 +18,19 @@ const ProductPrice: FC<IProductPrice> = ({  }) => {
                 <p className={styles.productPrice__discountPrice}>
                     <span className={styles.productPrice__discount}></span>
                 </p>
-                
+                <button className={styles.productPrice__favorite}>
+                    <Image src='/favourites.svg' width={20} height={18} alt={`Добавить в избранное`} />
+                </button>
+            </div>
+            <p className={styles.productPrice__originalPrice}></p>
+            <p className={styles.productPrice__bonuses}>
+                +339
+                <span className={styles.productPrice__bonusIcon}></span>
+                <span className={styles.productPrice__bonusText}></span>
+            </p>
+            <div className={styles.productPrice__btns}>
+                <ActionButton text="В корзину" onClick={() => null} />
+                <ActionButton text="Купить в 1 клик" onClick={() => null} />
             </div>
         </div>
     )
